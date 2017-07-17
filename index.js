1cd57f6bb4864986ef12fc8c700ef4fc7a872ab6
Jerry Holmes <jholmes@pagedragon.com>
2017-07-17 22:54:20 +0000

    Added landing page caterogry generation

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-hexo.extend.generator.register('directory-category', require('./lib/generator'));
+hexo.extend.generator.register('directory-category', require('./lib/page_generator'));
+hexo.extend.generator.register('landing-category', require('./lib/post_generator'));
